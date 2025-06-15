194a5a51645745e68cccf11b682e4475c1569d07
Muhammad Sohaib <sohaibkhansb9@gmail.com>
2025-06-15 09:12:09 +0500

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import streamlit as st
 import pandas as pd
 
+df = pd.DataFrame({A : [1,2], B : [3,4]})
+st.subheader(df)
+st.DataFrame(df)
+st.Table(df)
+st.json(df)
+"""
 st.header("header")
 st.subheader("subheader")
 st.text("text")
 st.code("print('python code')", language='python')
 st.markdown("**markdown**")
+"""
