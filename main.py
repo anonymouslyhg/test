51fa36bfd2619ea2066774b24a291005a8bfb905
Muhammad Sohaib <sohaibkhansb9@gmail.com>
2025-06-15 12:15:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,4 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 
-st.sidebar.title("Gamer")
-st.sidebar.radio("Option",["A","B","C"])
-col1, col2 = st.columns(2)
-
-with col1:
-  st.write("Left Columns")
-with col2:
-  st.write("Right Colimns")
-
-st.expander("See More").write("Hided Content")
+st.download("Download", data="Content of Data", file_name="data.txt")
