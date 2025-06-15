3b79ca71634cc7ef12c374aaff1e9697e0c15c59
Muhammad Sohaib <sohaibkhansb9@gmail.com>
2025-06-15 12:17:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 
-st.download("Download", data="Content of Data", file_name="data.txt")
+st.download_button("Download", data="Content of Data", file_name="data.txt")
 st.write("Donwloader")
