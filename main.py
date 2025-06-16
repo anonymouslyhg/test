b80bc6221ea918eb1ae405f6f82f778793b1c15f
Muhammad Sohaib <sohaibkhansb9@gmail.com>
2025-06-16 08:59:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import seaborn as sns
-import streamlit as st
-import pandas as pd
 import matplotlib.pyplot as plt
 
-df = sns.load_dataset("tips")
-
-st.set_page_config(page_title="Seaborn Graphs", layout="wide")
-st.title("Seaborn Demo")
-
-fig, ax = plt.subplots(figsize=(7, 3))
-sns.lineplot(x="total_bill", y="tip", data=df, ax=ax)
-st.pyplot(fig)
-
-st.header("Hello")
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
