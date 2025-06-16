0694b509f35258a84c8a8987ad0f6748917df74d
Muhammad Sohaib <sohaibkhansb9@gmail.com>
2025-06-16 12:59:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,7 @@ if uploaded_file is not None:
     # Read Excel using openpyxl engine
     file = pd.read_excel(uploaded_file, sheet_name='1', engine='openpyxl')
     gender = file["gender"].map({0: 'Female', 1: 'Male'})
-    st.text(gender[1])
+    st.text(gender)
+    for 'Male' in gender:
+        st.write('male')
 
