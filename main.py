ba51f81b40fb2f97f15a9d7f7bef71be24fdd797
Muhammad Sohaib <sohaibkhansb9@gmail.com>
2025-06-16 11:41:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import pandas as pd
 import streamlit as st
 
-# Load Excel file
-file = pd.read_excel("patient information.xlsx", sheet_name='Sheet1')
+st.title("Upload Excel File")
+uploaded_file = st.file_uploader("Choose a file", type=["xlsx"])
 
-# Show header
-st.subheader("Patient Data Preview")
-st.write(file.head())
-
-# Show basic info manually (since file.info() doesn't return a DataFrame)
-st.subheader("Data Information")
-buffer = []
-file.info(buf=buffer.append)
-st.text(''.join(buffer))
+if uploaded_file is not None:
+    file = pd.read_excel(uploaded_file, sheet_name='Sheet1')
+    st.subheader("Data Preview")
+    st.write(file.head())
