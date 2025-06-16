fd085c4d592c9830c2186fd1219d185b17c9926f
Muhammad Sohaib <sohaibkhansb9@gmail.com>
2025-06-16 11:54:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if uploaded_file is not None:
     
     st.subheader("Data Preview")
     st.write(file.head())
-    d_info = file.info()
+    d_info = file.isnull().sum()
     st.text(d_info)
 
 
