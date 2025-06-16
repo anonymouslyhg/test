8ec4f4454e3a92e038e2f172f5d69853a5097c76
Muhammad Sohaib <sohaibkhansb9@gmail.com>
2025-06-16 13:14:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx"])
 if uploaded_file is not None:
     # Rewind the file to start just to be safe
     uploaded_file.seek(0)
-    
+    fig = plt.figure()
     # Read Excel using openpyxl engine
     file = pd.read_excel(uploaded_file, sheet_name='1', engine='openpyxl')
     gender = file["gender"].map({0: 'Female', 1: 'Male'})
     gen_num = np.array(gender.value_counts())
     labels = ["Males", "Females"]
     plt.pie(gen_num, labels=labels)
-    
+    plt.show()
 
