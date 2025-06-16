565a399471329fe30f52c36bc47272db90a1c646
Muhammad Sohaib <sohaibkhansb9@gmail.com>
2025-06-16 12:54:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ if uploaded_file is not None:
     
     # Read Excel using openpyxl engine
     file = pd.read_excel(uploaded_file, sheet_name='1', engine='openpyxl')
-    date_errors = pd.to_datetime(file['Followbegintime'], errors='coerce')
-    st.text(date_errors.isna())
-
+    gender = file["gender"]
+    st.text(gender)
 
