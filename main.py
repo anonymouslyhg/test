9d960c5581fd869ae294bc5914eb1b265db18b83
Muhammad Sohaib <sohaibkhansb9@gmail.com>
2025-06-16 13:00:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ if uploaded_file is not None:
     # Read Excel using openpyxl engine
     file = pd.read_excel(uploaded_file, sheet_name='1', engine='openpyxl')
     gender = file["gender"].map({0: 'Female', 1: 'Male'})
-    st.text(gender)
+    st.text(gender.value_counts())
 
